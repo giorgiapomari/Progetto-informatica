942b0f1b0deaee5710f3a8bff4c2e273af51b936
Giorgia Pomari <giorgia.pomari@studenti.unitn.it>
2026-05-08 10:30:28 +0200

    first commit

---
+++ my-app/src/firestore.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYcb-GCJmkTjoC253mfj_BAQsyGFhn-W0",
+  authDomain: "progetto-informatica-452f0.firebaseapp.com",
+  projectId: "progetto-informatica-452f0",
+  storageBucket: "progetto-informatica-452f0.firebasestorage.app",
+  messagingSenderId: "85922112983",
+  appId: "1:85922112983:web:96c8f54e4201a6a0fa5792"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
