ee48c6f1542aceffed3cba55849516b0da108010
Giorgia Pajola <pajolagiorgia05@gmail.com>
2026-05-08 13:06:22 +0200

    Cambiato database

--- my-app/src/firestore.js
+++ my-app/src/firestore.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAYcb-GCJmkTjoC253mfj_BAQsyGFhn-W0",
-  authDomain: "progetto-informatica-452f0.firebaseapp.com",
-  projectId: "progetto-informatica-452f0",
-  storageBucket: "progetto-informatica-452f0.firebasestorage.app",
-  messagingSenderId: "85922112983",
-  appId: "1:85922112983:web:96c8f54e4201a6a0fa5792"
+  apiKey: "AIzaSyDQ8VWSWOZRnlHksjY6dCJzeYRH0sdtYws",
+  authDomain: "progetto-informatica-ricette.firebaseapp.com",
+  projectId: "progetto-informatica-ricette",
+  storageBucket: "progetto-informatica-ricette.firebasestorage.app",
+  messagingSenderId: "211008219757",
+  appId: "1:211008219757:web:14a60db3331f70dba02d64"
 };
 
 // Initialize Firebase
