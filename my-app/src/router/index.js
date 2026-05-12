734638b5731fb6a6b86ee50cc6b7731ede17bf92
Giorgia Pajola <pajolagiorgia05@gmail.com>
2026-05-12 12:12:12 +0200

    Fix: redirect automatico al login

--- my-app/src/router/index.js
+++ my-app/src/router/index.js
@@ -8,6 +8,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+    path: '/',
+    redirect: '/login' // <--- Aggiungi o modifica questa riga
+    },
+    {
       path: '/login',
       name: 'login',
       component: Login
