8443eec430b4a5202f17fec79cbf855abcdcfbe0
Giorgia Pomari <giorgia.pomari@studenti.unitn.it>
2026-05-15 10:10:08 +0200

    Aggiunta 18 apikey

--- my-app/src/config.js
+++ my-app/src/config.js
@@ -1,2 +1,9 @@
-export const API_KEY = 'cd31399f04b8490f99a11c2186f522e7'
+export const API_KEY = '3f5a6461b5c84a9ea4a604d84a26ecee'
 export const API_BASE_URL = 'https://api.spoonacular.com'
+
+/*
+75e2291920cc41e294381ec48ab73708
+a46caf99698849b7a7e0ee243aa47720
+b2fadeb5259645f8b3de8d7ed3d2ddec
+ed5056cf1d024adb9bf70b3eab86805f
+*/
